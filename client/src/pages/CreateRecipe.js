5ffec1bf57f5f01ce11c1852039f39899cbd149b
lsm0084 <heonmoo0@gmail.com>
2023-09-11 17:13:16 +0900

    사진업로드추가

--- client/src/pages/CreateRecipe.js
+++ client/src/pages/CreateRecipe.js
@@ -9,11 +9,60 @@ const InputContainer = styled.div`
   justify-content: center;
   width: 100%;
   align-items: center;
-  height: 100vh;
+  .postBtn {
+    margin-bottom: 30px;
+  }
+  .fileInput {
+    opacity: 0;
+    width: 0.1px;
+    height: 0.1px;
+    position: absolute;
+    overflow: hidden;
+    z-index: -1;
+  }
+  .custom-label {
+    background-color: #3498db;
+    color: #fff;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover {
+      background-color: rgba(0, 109, 205, 1);
+    }
+  }
+  .fileInput + .custom-label {
+    display: inline-block;
+  }
+  .fileInput + .custom-label + span {
+    display: none;
+  }
+
+  .Crtitle {
+    font-weight: bold;
+    font-size: 50px;
+    padding: 10px;
+    border-bottom: 1px solid rgba(181, 181, 181, 1);
+    width: 80%;
+    margin-top: 60px;
+  }
+  .recipe-form {
+    border: none;
+    width: 600px;
+    height: 323px;
+    margin: 10px;
+    text-align: center;
+    background-color: rgba(217, 217, 217, 1);
+  }
+  .fileInput {
+    margin-bottom: 25px;
+  }
+  .preview-image {
+    max-width: 100%;
+    z-index: 100000;
+  }
   .error-message {
     color: red;
-    margin-top: 10px;
-    margin-left: 10px;
+    margin-bottom: 20px;
     font-size: 14px;
   }
   .custom-select {
@@ -23,41 +72,58 @@ const InputContainer = styled.div`
     width: 200px;
   }
   .NameInput {
-    padding: 10px;
+    margin-top: 10px;
     margin-bottom: 30px;
+    margin-right: 100px;
     display: flex;
+    font-weight: bold;
     > input {
       margin-left: 20px;
       height: 30px;
-      width: 600px;
+      width: 590px;
+      border: 8px solid rgba(217, 217, 217, 1);
+      border-radius: 8px;
     }
   }
   .CategoryInput {
     padding: 10px;
     display: flex;
-    margin-bottom: 30px;
+    margin-bottom: 10px;
+    font-weight: bold;
+    margin-right: 120px;
     > select {
       margin-left: 20px;
-      width: 630px;
+      width: 610px;
+      height: 60px;
+      border: 8px solid rgba(217, 217, 217, 1);
+      border-radius: 8px;
     }
   }
   .NutrientInput {
     padding: 10px;
     display: flex;
-    margin-bottom: 30px;
+    margin-bottom: 10px;
+    font-weight: bold;
+    margin-right: 90px;
     > textarea {
       margin-left: 20px;
       height: 30px;
-      width: 600px;
+      width: 590px;
+      border: 8px solid rgba(217, 217, 217, 1);
+      border-radius: 8px;
     }
   }
   .PlanInput {
     padding: 10px;
     display: flex;
+    font-weight: bold;
+    margin-right: 110px;
     > textarea {
-      height: 100px;
+      height: 150px;
       margin-left: 20px;
-      width: 625px;
+      width: 590px;
+      border: 8px solid rgba(217, 217, 217, 1);
+      border-radius: 8px;
     }
   }
   .section {
@@ -73,6 +139,7 @@ const InputContainer = styled.div`
     height: 60px;
     font-size: 20px;
     color: white;
+    cursor: pointer;
     &:hover {
       background-color: rgba(0, 109, 205, 1);
     }
@@ -84,7 +151,9 @@ const CreateRecipe = () => {
   const [nutrients, setNutrients] = useState('');
   const [cookingMethod, setCookingMethod] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
   const [error, setError] = useState('');
+  const [imagePreviewUrl, setImagePreviewUrl] = useState('');
 
   const navigate = useNavigate();
 
@@ -104,43 +173,81 @@ const CreateRecipe = () => {
     return selectedCategory !== '';
   };
 
-  const handleRecipeSubmit = () => {
+  const handleImageSelect = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedImage(file);
+
+      const imageUrl = URL.createObjectURL(file);
+      setImagePreviewUrl(imageUrl);
+    }
+  };
+
+  const handleRecipeSubmit = async () => {
     if (
       !validateRecipeName() ||
       !validateNutrients() ||
       !validateCookingMethod() ||
       !validateSelectedCategory() ||
-      /[!@#$%^&*(),.?":{}|<>]/.test(recipeName)
+      /[!@#$%^&*(),.?":{}|<>]/.test(recipeName) ||
+      !selectedImage
     ) {
-      setError('모든 필드에 특수문자를 제외한 값을 채워주세요');
+      setError(
+        '모든 필드에 특수문자를 제외한 값을 채워주세요 및 이미지를 선택하세요',
+      );
       return;
     }
 
     setError('');
 
-    const recipeData = {
-      recipeTitle: recipeName,
-      nutrition: nutrients,
-      recipeBody: cookingMethod,
-      recipeType: selectedCategory,
-    };
+    const recipeData = new FormData();
+    recipeData.append('memberId', '1');
+    recipeData.append('recipeTitle', recipeName);
+    recipeData.append('recipeType', selectedCategory);
+    recipeData.append('nutrition', nutrients);
+    recipeData.append('recipeBody', cookingMethod);
+    recipeData.append('image', selectedImage);
 
     const apiUrl = process.env.REACT_APP_API_URL;
 
-    axios
-      .post(`${apiUrl}/recipes`, recipeData)
-      .then((response) => {
-        console.log('Recipe created:', response.data);
-        navigate('/mainpage');
-      })
-      .catch((error) => {
-        console.error('Error creating recipe:', error);
+    try {
+      const response = await axios.post(apiUrl + '/recipes', recipeData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       });
+      console.log('Recipe created:', response.data);
+      navigate('/recipes/main');
+    } catch (error) {
+      console.error('Error creating recipe:', error);
+    }
   };
 
   return (
     <>
       <InputContainer>
+        <div className="Crtitle">레시피 작성하기</div>
+        <div className="recipe-form">
+          {imagePreviewUrl && (
+            <div className="image-preview">
+              <img
+                src={imagePreviewUrl}
+                alt="미리보기"
+                className="preview-image"
+              />
+            </div>
+          )}
+        </div>
+        <input
+          id="fileInput"
+          className="fileInput"
+          type="file"
+          accept="image/*"
+          onChange={handleImageSelect}
+        />
+        <label htmlFor="fileInput" className="custom-label">
+          파일 선택
+        </label>
         <div className="NameInput">
           <div className="section">요리명</div>
           <input
@@ -148,10 +255,10 @@ const CreateRecipe = () => {
             onChange={(e) => setRecipeName(e.target.value)}
             placeholder="ex) 새우두부계란찜"
           />
-          {!validateRecipeName() && (
-            <div className="error-message">요리명을 입력하세요.</div>
-          )}
         </div>
+        {!validateRecipeName() && (
+          <div className="error-message">요리명을 입력하세요.</div>
+        )}
         <div className="CategoryInput">
           <div className="section">요리종류</div>
           <select
@@ -168,10 +275,10 @@ const CreateRecipe = () => {
             <option value="밥">밥</option>
             <option value="국&찌개">국&찌개</option>
           </select>
-          {!validateSelectedCategory() && (
-            <div className="error-message">요리종류를 선택하세요.</div>
-          )}
         </div>
+        {!validateSelectedCategory() && (
+          <div className="error-message">요리종류를 선택하세요.</div>
+        )}
         <div className="NutrientInput">
           <div className="section">영양분</div>
           <textarea
@@ -179,10 +286,10 @@ const CreateRecipe = () => {
             onChange={(e) => setNutrients(e.target.value)}
             placeholder="ex) 지방 15g, 나트륨 30g"
           />
-          {!validateNutrients() && (
-            <div className="error-message">영양분 정보를 입력하세요.</div>
-          )}
         </div>
+        {!validateNutrients() && (
+          <div className="error-message">영양분 정보를 입력하세요.</div>
+        )}
         <div className="PlanInput">
           <div className="section">조리방법</div>
           <textarea
@@ -190,12 +297,14 @@ const CreateRecipe = () => {
             onChange={(e) => setCookingMethod(e.target.value)}
             placeholder="ex) 1. 손질된 새우를 끓는 물에 데쳐 건진다"
           />
-          {!validateCookingMethod() && (
-            <div className="error-message">조리방법을 입력하세요.</div>
-          )}
         </div>
+        {!validateCookingMethod() && (
+          <div className="error-message">조리방법을 입력하세요.</div>
+        )}
         {error && <div className="error-message">{error}</div>}
-        <button onClick={handleRecipeSubmit}>레시피 생성</button>
+        <button className="postBtn" onClick={handleRecipeSubmit}>
+          레시피 생성
+        </button>
       </InputContainer>
     </>
   );
