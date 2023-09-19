97d6b5fc2a5d6ef4e361d231b946678635e11678
KimYMin <minmin4439@naver.com>
2023-09-19 17:48:30 +0900

    충돌

--- client/src/components/MainPageCompo/MainList.js
+++ client/src/components/MainPageCompo/MainList.js
@@ -23,9 +23,9 @@ function MainList({ currentData }) {
         {Object.values(currentData).map((el) => {
           return (
             <FoodData
-              key={el.RCP_SEQ}
+              key={el.id}
               picture={el.ATT_FILE_NO_MAIN}
-              title={el.RCP_NM}
+              title={el.recipeTitle}
             ></FoodData>
           );
         })}
@@ -39,3 +39,44 @@ MainList.propTypes = {
 };
 
 export default MainList;
+// import { styled } from 'styled-components';
+// import PropTypes from 'prop-types';
+// import FoodData from './FoodData';
+
+// const Container = styled.div`
+//   display: flex;
+//   justify-content: center;
+// `;
+
+// const ListContainer = styled.ul`
+//   display: grid;
+//   grid-template-columns: repeat(4, 350px);
+//   justify-content: center;
+//   list-style: none;
+//   padding-top: 10px;
+// `;
+
+// // currentData 값을 출력
+// function MainList({ currentData }) {
+//   return (
+//     <Container>
+//       <ListContainer>
+//         {Object.values(currentData).map((el) => {
+//           return (
+//             <FoodData
+//               key={el.RCP_SEQ}
+//               picture={el.ATT_FILE_NO_MAIN}
+//               title={el.RCP_NM}
+//             ></FoodData>
+//           );
+//         })}
+//       </ListContainer>
+//     </Container>
+//   );
+// }
+
+// MainList.propTypes = {
+//   currentData: PropTypes.array.isRequired,
+// };
+
+// export default MainList;
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -51,8 +51,10 @@ function MainPage() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(``);
-      setData(response.data.COOKRCP01.row);
+      const response = await axios.get(
+        `https://c6c5-45-64-145-74.ngrok-free.app/recipes`,
+      );
+      setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -67,8 +69,8 @@ function MainPage() {
       setFilterData(data);
     } else {
       setFilterData(
-        data.filter((el) => {
-          return el.RCP_PAT2 === obj.name;
+        Object.values(data).filter((el) => {
+          return el.recipeType === obj.name;
         }),
       );
     }
@@ -80,11 +82,11 @@ function MainPage() {
     setSearchData(typing);
 
     setFilterData(
-      data.filter((el) => {
+      Object.values(data).filter((el) => {
         return obj.name === '전체'
-          ? el.RCP_NM.toLowerCase().includes(typing)
-          : el.RCP_NM.toLowerCase().includes(typing) &&
-              el.RCP_PAT2 === obj.name;
+          ? el.recipeTitle.toLowerCase().includes(typing)
+          : el.recipeTitle.toLowerCase().includes(typing) &&
+              el.recipeType === obj.name;
       }),
     );
   };
@@ -108,3 +110,116 @@ function MainPage() {
 }
 
 export default MainPage;
+// import { useState, useEffect, useMemo } from 'react';
+// import { styled } from 'styled-components';
+// import axios from 'axios';
+// import Category from '../components/MainPageCompo/Category';
+// import MainList from '../components/MainPageCompo/MainList';
+// import Paging from '../components/MainPageCompo/Paging';
+// import Search from '../components/MainPageCompo/Search';
+// import { useParams } from 'react-router-dom';
+
+// const PageContainer = styled.div``;
+
+// function MainPage() {
+//   const [data, setData] = useState([]); // API로 받아온 모든 데이터
+//   const [filterData, setFilterData] = useState([]); // 카테고리 변경으로 인한 data 변경 값
+//   const [currentData, setCurrentData] = useState([]); // 페이지 변경으로 인한 fillterData 변경 값
+//   const [searchData, setSearchData] = useState(''); // 검색 내용
+
+//   //카테고리, 페이지 값 저장
+//   const obj = useParams();
+//   console.log(obj);
+//   console.log(obj.page);
+//   console.log(currentData);
+//   console.log(filterData);
+//   console.log(data);
+
+//   const start =
+//     obj.page === undefined ? 0 : obj.page === 'page' ? 0 : (obj.page - 1) * 12;
+//   const end = start + 12;
+
+//   const menu = useMemo(
+//     () => [
+//       { id: 1, name: '전체', content: 'all', picture: 'picture/food.png' },
+//       { id: 2, name: '밥', content: 'rice', picture: 'picture/rice.png' },
+//       { id: 3, name: '반찬', content: 'sidedish', picture: 'picture/side.png' },
+//       { id: 4, name: '국&찌개', content: 'soup', picture: 'picture/soup.png' },
+//       {
+//         id: 5,
+//         name: '일품',
+//         content: 'maindish',
+//         picture: 'picture/noodle.png',
+//       },
+//       {
+//         id: 6,
+//         name: '후식',
+//         content: 'dessert',
+//         picture: 'picture/dessert.png',
+//       },
+//     ],
+//     [],
+//   );
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get(
+//         `https://cd98-45-64-145-74.ngrok-free.app/recipes`,
+//       );
+//       setData(response.data);
+//       console.log(data);
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   };
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   // 카테고리로 인한 data 분류
+//   useEffect(() => {
+//     if (obj.name === '전체' || obj.name === 'name' || obj.name === undefined) {
+//       setFilterData(data);
+//     } else {
+//       setFilterData(
+//         Object.values(data).filter((el) => {
+//           return el.RCP_PAT2 === obj.name;
+//         }),
+//       );
+//     }
+//   }, [data, obj.name]);
+
+//   //검색으로 인한 분류
+//   const HandleOnSearch = (e) => {
+//     const typing = e.target.value.toLowerCase();
+//     setSearchData(typing);
+
+//     setFilterData(
+//       Object.values(data).filter((el) => {
+//         return obj.name === '전체'
+//           ? el.RCP_NM.toLowerCase().includes(typing)
+//           : el.RCP_NM.toLowerCase().includes(typing) &&
+//               el.RCP_PAT2 === obj.name;
+//       }),
+//     );
+//   };
+
+//   // 페이지 변경으로 인한 fillterData 분류
+//   useEffect(() => {
+//     if (filterData.length > 0) {
+//       const newCurrentData = filterData.slice(start, end);
+//       setCurrentData(newCurrentData);
+//     }
+//   }, [filterData, start, end]);
+
+//   return (
+//     <PageContainer>
+//       <Search searchData={searchData} HandleOnSearch={HandleOnSearch} />
+//       <Category menu={menu} obj={obj} setSearchData={setSearchData}></Category>
+//       <MainList currentData={currentData}></MainList>
+//       <Paging filterData={filterData} obj={obj} />
+//     </PageContainer>
+//   );
+// }
+
+// export default MainPage;
